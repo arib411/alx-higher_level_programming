f3acb409aa1513f754aa454a4b5418285c408248
arib411 <soufian.arib@gmail.com>
2023-11-14 21:46:24 +0100

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,32 @@
-#!/usr/bin/node
-const fs = require('fs');
+#!/usr/bin/nodeconst
+fs = require('fs');
 
-const fArg = fs.readFileSync(process.argv[2]).toString();
-const sArg = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], fArg + sArg);
+function concatFiles(file1Path, file2Path, destinationPath) {
+  try {
+    // Read the content of the first file
+    const file1Content = fs.readFileSync(file1Path, 'utf8');
+
+    // Read the content of the second file
+    const file2Content = fs.readFileSync(file2Path, 'utf8');
+
+    // Concatenate the content of the two files
+    const concatenatedContent = file1Content + file2Content;
+
+    // Write the concatenated content to the destination file
+    fs.writeFileSync(destinationPath, concatenatedContent, 'utf8');
+
+    console.log('Files concatenated successfully!');
+  } catch (error) {
+    console.error('Error concatenating files:', error.message);
+  }
+}
+
+// Command-line arguments
+const [,, file1Path, file2Path, destinationPath] = process.argv;
+
+// Check if all required arguments are provided
+if (file1Path && file2Path && destinationPath) {
+  concatFiles(file1Path, file2Path, destinationPath);
+} else {
+  console.error('Usage: node concatFiles.js <file1Path> <file2Path> <destinationPath>');
+}
