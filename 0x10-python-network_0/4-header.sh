2be6a0fadbed9e5e5d78daae4767cb3da5c8c6b5
arib411 <soufian.arib@gmail.com>
2023-11-30 22:58:21 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Set the URL and header values
-url=$1
-header="X-School-User-Id: 98"
-
-# Send GET request with specified header and display the body
-curl -H "$header" "$url"
+# Script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response, A header variable X-HolbertonSchool-User-Id must be sent with the value 98
+curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
